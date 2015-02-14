4b1d290b8bdda90dfb4ecf91cbcc8506e468328a
Federico C. Repond <frepond@gmail.com>
2015-02-14 17:59:43 -0300

    Changed canvas size, move distance and title. Added support for node.

--- src/js/move.js
+++ src/js/move.js
@@ -1,6 +1,8 @@
-﻿	function moveUp(){
-		if(game.bomb.y - 4 >= 0){
-			game.bomb.y -= 4;
+﻿var inc = 16;
+
+	function moveUp(){
+		if(game.bomb.y - inc >= 0){
+			game.bomb.y -= inc;
 		}
 		else{
 			game.bomb.y -= 0;	
@@ -8,8 +10,8 @@
 	}
 	
 	function moveDown(){
-		if(game.bomb.y + 4 <= 400){
-			game.bomb.y += 4;
+		if(game.bomb.y + inc <= 400){
+			game.bomb.y += inc;
 		}
 		else{
 			game.bomb.y = 400;
@@ -17,8 +19,8 @@
 	}
 	
 	function moveLeft(){
-		if(game.bomb.x -4 >= 0){
-			game.bomb.x -= 4;
+		if(game.bomb.x - inc >= 0){
+			game.bomb.x -= inc;
 		}
 		else{
 			game.bomb.x = 0;
@@ -26,8 +28,8 @@
 	}
 	
 	function moveRight(){
-		if(game.bomb.x + 4 <= 620){
-			game.bomb.x += 4;
+		if(game.bomb.x + inc <= 620){
+			game.bomb.x += inc;
 		}
 		else{
 			game.bomb.x = 620;
