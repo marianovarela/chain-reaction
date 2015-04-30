226a9782186f48f8fee273668bcbb11f8d4a1ec0
mvarela <marianogvarela@gmail.com>
2015-04-30 12:04:00 -0300

    game server url

--- src/js/game.js
+++ src/js/game.js
@@ -7,8 +7,8 @@
 	var $GAME = {};
 	
 	$GAME.name = "CHAIN_REACTION";
-	// $GAME.server = "http://tiendatac.minplan.gob.ar/games-score";
-	$GAME.server = "http://localhost:8083";
+	$GAME.server = "http://tiendatac.minplan.gob.ar/games-score";
+	// $GAME.server = "http://localhost:8083";
 	$GAME.url_get = $GAME.server + "/api/v1/score/search/findByGameOrderByScoreDesc?game=";
 	$GAME.url_post = $GAME.server + "/api/v1/score";
 	
